46c04f5f71b6057633950cf916c153d63380e641
Laurens Krombeen <l.krombeen@mindefnl.onmicrosoft.com>
2026-05-15 11:25:05 +0200

    Add basic flow unit tests

--- 3kfc/src/main/java/com/example/_kfc/person/entities/PeopleRecord.java
+++ 3kfc/src/main/java/com/example/_kfc/person/entities/PeopleRecord.java
@@ -4,9 +4,10 @@ import com.example._kfc.person.domain.valueobjects.Person;
 import com.example._kfc.person.dtos.PersonDto;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class PeopleRecord {
-    private final HashMap<Integer, Person> record = new HashMap<>();
+    private final Map<Integer, Person> record = new HashMap<>();
 
     /**
      * Add a person to the registry
--- 3kfc/src/test/java/com/example/_kfc/person/entities/PersonTest.java
+++ 3kfc/src/test/java/com/example/_kfc/person/entities/PersonTest.java
@@ -1,15 +1,82 @@
 package com.example._kfc.person.entities;
 
-import com.example._kfc.person.domain.valueobjects.Person;
+import com.example._kfc.person.dtos.PersonDto;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PersonTest {
-    @Test
-    void testHasPartner() {
-        var bachelor = new Person.PersonBuilder().id(1).name("Han Solo").birthDate("1942-07-13").build();
+    private final PersonDto Han = new PersonDto(5, "Han Solo", "1942-07-13",
+            new PersonDto.RelatedPersonDto(1),
+            new PersonDto.RelatedPersonDto(2),
+            new PersonDto.RelatedPersonDto(6),
+            new ArrayList<>());
+    private final PersonDto Leia = new PersonDto(6, "Leia Organa", "1942-07-13",
+            new PersonDto.RelatedPersonDto(3),
+            new PersonDto.RelatedPersonDto(4),
+            new PersonDto.RelatedPersonDto(5),
+            new ArrayList<>());
+    private final PersonDto Ben = new PersonDto(5, "Ben Solo", "1966-01-21",
+            new PersonDto.RelatedPersonDto(5),
+            new PersonDto.RelatedPersonDto(6),
+            new PersonDto.RelatedPersonDto(7),
+            new ArrayList<>());
+    private final PersonDto Jacen = new PersonDto(5, "Jacen Solo", "1967-01-21",
+            new PersonDto.RelatedPersonDto(5),
+            new PersonDto.RelatedPersonDto(6),
+            new PersonDto.RelatedPersonDto(7),
+            new ArrayList<>());
+    private final PersonDto Jaina = new PersonDto(5, "Jaina Solo", "1968-01-21",
+            new PersonDto.RelatedPersonDto(5),
+            new PersonDto.RelatedPersonDto(6),
+            new PersonDto.RelatedPersonDto(7),
+            new ArrayList<>());
 
-        assertFalse(bachelor.HasPartner());
+    @Test
+    void newPersonReturnsFalse() {
+        var record = new PeopleRecord();
+
+        var actual = record.AddPerson(Han);
+
+        assertFalse(actual);
+    }
+
+    @Test
+    void newPersonWithPartnerButNoChildrenReturnsFalse() {
+        var record = new PeopleRecord();
+
+        record.AddPerson(Han);
+        var actual = record.AddPerson(Leia);
+
+        assertFalse(actual);
+    }
+
+    @Test
+    void newPersonWithPartnerButNoAdultChildrenReturnsFalse() {
+        var record = new PeopleRecord();
+
+        record.AddPerson(Han);
+        record.AddPerson(Leia);
+        record.AddPerson(Ben);
+        record.AddPerson(Jacen);
+        var actual = record.AddPerson(Jaina);
+
+        assertFalse(actual);
+    }
+
+    @Test
+    void newPersonWithPartnerButWithAdultChildrenReturnsTrue() {
+        var record = new PeopleRecord();
+
+        record.AddPerson(Han);
+        record.AddPerson(Leia);
+        record.AddPerson(Ben);
+        record.AddPerson(Jacen);
+        var actual = record.AddPerson(Jaina);
+
+        assertTrue(actual);
     }
 }
